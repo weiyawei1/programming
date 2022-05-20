b5e3c0063cef715309f4ad85f5e684bd578d932a
weiyawei1 <wei_yawei@sina.cn>
2022-05-20 20:00:36 +0800

    updata test.py

--- motif_FFM_CD/test.py
+++ motif_FFM_CD/test.py
@@ -9,6 +9,7 @@ Created on Wed Jan 17 09:55:19 2022
    使用各种优化算法，基于模体的加权网络的社区检测
 """
 import os
+import numpy as np
 
 file_path = r"E:\weiyawei\workspace\programming\motif_FFM_CD\logs\lesmis\5_Qc_FCD_log.txt"
 NWMMlist = []
@@ -26,6 +27,17 @@ with open(file_path, mode='r') as f:
                 NMMlist.append(line.split('=')[1][:-1])
             elif line.find('NOMM') != -1:
                 NOMMlist.append(line.split('=')[1][:-1])
+                
+NWMM_mean = np.asarray(NWMMlist, dtype=float).mean()
+MNMM_mean = np.asarray(MNMMlist,dtype=float).mean()
+NMM_mean = np.array(NMMlist,dtype=float).mean()
+NOMM_mean = np.array(NOMMlist, dtype=float).mean()
+print("NWMM_mean=",NWMM_mean)
+print("MNMM_mean=",MNMM_mean)
+print("NMM_mean=",NMM_mean)
+print("NOMM_mean=",NOMM_mean)
+
+
 
 
 
